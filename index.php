2f49c1b499b4a9b7c49c2872cf5e0de7f60ab40d
Hastur1437 <147645812+Hastur1437@users.noreply.github.com>
2023-10-16 22:46:11 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Главная</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+<div class="bgded" style="background-image:url('images/demo/backgrounds/01.png');">
+  <div class="wrapper overlay">
+    <header id="header" class="hoc clear">
+      <nav id="mainav" class="clear">
+        <ul class="clear">
+          <li><a href="index.php">Главная</a></li>
+          <li><a class="drop" href="#">Каталог</a>
+            <ul>
+              <li><a href="cpu.php">Процессоры</a></li>
+              <li><a href="motherboard.php">Материнские платы</a></li>
+              <li><a href="video-card.php">Видео-карты</a></li>
+            </ul>
+          <li><a href="delivery.php">Доставка</a></li>
+          <li><a href="#">Компания</a></li>
+          <li><a href="account.php">Личный кабнет</a></li>
+        </ul>
+      </nav>
+      <div id="logo">
+        <h1><a href="index.php">PC/LAB</a></h1>
+        <p>мы создаём будущее</p>
+      </div>
+    </header>
+  </div>
+
+  <div id="pageintro" class="hoc clear">
+    <article>
+      <div class="introtxt">
+        <p class="font-xs nospace">Сборка, апгрейд и ремонт компьютеров</p>
+        <h2 class="heading">PC/LAB</h2>
+        <p>Создавать лучшие компьютеры, которые будут дарить
+          геймерам и творческим профессионалам удовольствие от использования. PC/LAB это
+          восхитительный дизайн, высокая производительность,
+          безупречное качество и персональный сервис.</p>
+      </div>
+      <footer>
+        <ul class="nospace inline pushright">
+          <li><a class="btn inverse" href="delivery.php">Доставка</a></li>
+          <li><a class="btn" href="#">Компания</a></li>
+        </ul>
+      </footer>
+    </article>
+  </div>
+</div>
+    <div class="clear"></div>
+<div class="bgded overlay" style="background-image:url('images/demo/backgrounds/01.png');">
+  <section id="testimonials" class="hoc container clear">
+    <div class="center btmspace-50">
+      <h2 class="heading">Наша миссия</h2>
+      <p>Официальные партнеры NVIDIA, Intel и Microsoft.
+        Только лучшие комплектующие и передовые технологии.
+        Профессиональная сборка и кастомизация.
+        Специальное стресс-тестирование и установка ПО.
+        Доставка, установка и настройка компьютера под ключ.
+        Собственное производство.
+        Шоурум в Москве с компьютерами в наличии.
+        Сервисные центры в более чем 40 крупных городах России.</p>
+    </div>
+    <ul class="nospace group">
+      <li class="one_half first">
+        <div class="clear"><img src="images/demo/80x80.png" alt=""> <span class="block"><strong>Hastur</strong> / <em>Вебдев и т.д</em></span>
+          <blockquote>Я Орочимару,по классу воин, по жизни разбойник,с девками клирик,с братками мечник,с мусорами нищий,какие ко мне претензии?</blockquote>
+        </div>
+      </li>
+    </ul>
+  </section>
+</div>
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear">
+    <p class="fl_left">Создано 2022 - Создатель - <a href="#">Hastur</a></p>
+    <p class="fl_right">Create by <a href="#">Hastur</a></p>
+  </div>
+</div>
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+<script src="layout/scripts/jquery.placeholder.min.js"></script>
+</body>
+</html>
